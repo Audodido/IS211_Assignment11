e565a90d9f85e04ef0328f3524f0be711575b317
connor <chanwick@gmail.com>
2021-11-11 17:49:40 -0500

    some styling

--- todoapp.py
+++ todoapp.py
@@ -1,28 +1,13 @@
 from os import error
 from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
+from os.path import exists
 import re
+
 
 app = Flask(__name__) 
 
-tasks = [ #list of dictionaries containing required info abt each task
-
-    {
-        'task' : 'get milk', 
-        'assigned' : 'connor@mail.com', 
-        'priority' : 'low'
-    },
-    {
-        'task' : 'cook dinner', 
-        'assigned' : 'peggy@mail.com', 
-        'priority' : 'high'
-    },
-    {
-        'task' : 'do dishes', 
-        'assigned' : 'monk@mail.com', 
-        'priority' : 'medium'
-    }
-]
+tasks = [] #list of dictionaries containing required info abt each task
 
 @app.route('/') 
 def display_list():
@@ -34,11 +19,26 @@ def error_message(error):
     return render_template('error.html', error=error)
 
 
+@app.route('/save', methods=['POST'])
+def save_list():
+    with open('list_archive.txt', 'w+', encoding='utf-8') as f:
+        for t in tasks:
+            f.writelines(f"{t['task']}, {t['assigned']}, {t['priority']}\n")
+    return redirect('/')
+
+
 @app.route('/clear', methods=['POST'])
 def clear_table():
     tasks.clear()
     return redirect('/')
     
+    
+## Extra credit II
+# @app.route('/delete', methods=['POST'])
+# def delete_row():
+#     to_go = request.form['']
+#     tasks.append({'to go' : to_go})
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -56,13 +56,24 @@ def submit():
             # return render_template('post.html')
             return redirect('/')
         else:
-        #     return "Please enter valid priority level" #these errors can be same page just change variable
             error = 'priority'
     else:
-        # return "Please enter a valid email address" #change to an error page w return button
         error = 'email'
         return redirect(url_for('error_message', error=error))
         
 
-if __name__ == '__main__': 
-    app.run(debug=True)
+if __name__ == '__main__':
+
+    if exists('list_archive.txt'):
+        with open ('list_archive.txt', 'r') as f:
+            lines = f.readlines()
+            if len(lines) > 0:
+                for line in lines:
+                    entry = line.split(',')
+                    tasks.append({
+                        'task' : entry[0],
+                        'assigned' : entry[1],
+                        'priority' : entry[2]})
+                
+
+        app.run(debug=True)
